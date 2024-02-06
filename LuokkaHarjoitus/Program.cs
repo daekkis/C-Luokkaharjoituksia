87ef41ecacd3636bbf52e68c5cf26045ec6a962a
Daekkis <51443@edu.raseko.fi>
2024-02-06 14:29:31 +0200

    Added a constructor with 3 overloads

--- LuokkaHarjoitus/Program.cs
+++ LuokkaHarjoitus/Program.cs
@@ -6,15 +6,44 @@ using System.Threading.Tasks;
 
 namespace LuokkaHarjoitus
 {
-    internal class Program
+    
     {
         class Hooman
         {
-            // Define properties of Hooman
-            string name;
-            int age;
-            string gender;
+            // Define properties of Hooman ie. fields
+            string name = "Essi Esimerkki";
+            int age = 30;
+            string gender = "Emäntä";
+
+            Hooman()
+            {
+                
+            }
+
+            // Constructor with one argument
+
+            Hooman(string name)
+            {
+                this.name = name;
+            }
+
+            // Constructor wiht 2 agruments
+            Hooman(string name, int age) 
+            {
+                this.name = name;
+                this.age = age;
+            }
+
+            // Constructor with 3 agruments
+            Hooman(string name, int age, string gender) 
+            {
+            this.name = name;
+            this.age = age;
+            this.gender = gender;
+            }
         }
+        internal class Program
+        { }
         static void Main(string[] args)
         {
         }
