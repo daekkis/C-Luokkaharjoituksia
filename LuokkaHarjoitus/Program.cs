68c44a337d9c9a2c7f51e89cbf015095ac1cdd12
Daekkis <51443@edu.raseko.fi>
2024-02-06 15:18:22 +0200

    Created an object from Hooman class in the Main of Program class and tested it.

--- LuokkaHarjoitus/Program.cs
+++ LuokkaHarjoitus/Program.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace LuokkaHarjoitus
 {
     
-    {
+   
         class Hooman
         {
             // Define properties of Hooman ie. fields
@@ -15,37 +15,57 @@ namespace LuokkaHarjoitus
             int age = 30;
             string gender = "Emäntä";
 
-            Hooman()
+            // Default constructor w/o agruments
+            // No need to define, will be created automatically
+            public Hooman()
             {
                 
             }
 
             // Constructor with one argument
 
-            Hooman(string name)
+            public Hooman(string name)
             {
                 this.name = name;
             }
 
             // Constructor wiht 2 agruments
-            Hooman(string name, int age) 
+            public Hooman(string name, int age) 
             {
                 this.name = name;
                 this.age = age;
             }
 
             // Constructor with 3 agruments
-            Hooman(string name, int age, string gender) 
+            public Hooman(string name, int age, string gender) 
             {
-            this.name = name;
-            this.age = age;
-            this.gender = gender;
+                this.name = name;
+                this.age = age;
+                this.gender = gender;
+
+            }
+
+            // A method to say something 
+            public void SayOpinion()
+            {
+            Console.WriteLine("Voi lemmikit ne on elämän suola");
             }
         }
         internal class Program
-        { }
-        static void Main(string[] args)
-        {
-        }
-    }
+        { 
+    
+            static void Main(string[] args)
+            {
+               // Create (instantiate) a hooman object from Hooman class
+                Hooman owner = new Hooman("Ossi Omistaja", 35, "isäntä");
+
+                // Call the SayOpinion method
+                owner.SayOpinion();
+
+                // Keep the window open until enter pressed
+
+                Console.ReadLine();
+            
+            }
+        } 
 }
