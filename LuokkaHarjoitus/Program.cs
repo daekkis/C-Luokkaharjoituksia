d81027e5b01953346dabaee669e47a10abd08bcf
Daekkis <51443@edu.raseko.fi>
2024-02-06 14:02:40 +0200

    Started creating a new class for pet owner. Basic fields designed.

---
+++ LuokkaHarjoitus/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuokkaHarjoitus
+{
+    internal class Program
+    {
+        class Hooman
+        {
+            // Define properties of Hooman
+            string name;
+            int age;
+            string gender;
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
