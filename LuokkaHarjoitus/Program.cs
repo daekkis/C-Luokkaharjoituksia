f7868300563f3b10e73ccc511b728abe90c34f4f
Admin <51443@edu.raseko.fi>
2024-02-13 10:26:42 +0200

    final code

--- LuokkaHarjoitus/Program.cs
+++ LuokkaHarjoitus/Program.cs
@@ -4,95 +4,116 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LuokkaHarjoitus
+namespace Luokkaharjoituksia1
 {
-    
- 
-        class Hooman
+    // A superclass for various pet animals
+    class Pet
+    {
+        // Eats method is explicitly defind to be over written in the subclass with keyword virtual
+        public virtual void Eats()
         {
-            // Define properties of Hooman ie. fields
-            public string name = "Essi Esimerkki";
-            public int age = 30;
-            public string gender = "Emäntä";
-
-            // Default constructor w/o agruments
-            // No need to define, will be created automatically
-            public Hooman()
-            {
-                
-            }
-
-            // Constructor with one argument
-
-            public Hooman(string name)
-            {
-                this.name = name;
-            }
-
-            // Constructor wiht 2 agruments
-            public Hooman(string name, int age) 
-            {
-                this.name = name;
-                this.age = age;
-            }
-
-            // Constructor with 3 agruments
-            public Hooman(string name, int age, string gender) 
-            {
-                this.name = name;
-                this.age = age;
-                this.gender = gender;
-
-            }
-
-            // A method to say something 
-            public void SayOpinion()
-            {
-            Console.WriteLine("Voi lemmikit ne on elämän suola");
-            }
+            Console.WriteLine("Eats some food");
         }
+    }
 
-        class CatOwner : Hooman
+    // A subcalss of pet
+    class Hare : Pet
+    {
+        // Override allows over witining base class methos
+        public override void Eats()
         {
+            Console.WriteLine("Eats carrots and grass");
+        }
+    }
 
-            public void SayOpinion()
-            {
-                Console.WriteLine("Kissat ovat itsenäisiä ja pitivät hiiret loitolla"); 
-            }
+    class Hooman
+    {
+        // Define properties of Hooman ie. fields
+        public string name = "Essi Esimerkki";
+        public int age = 30;
+        public string gender = "Emäntä";
+
+        // Default constructor w/o arguments
+        // No need to define, will be crated automatically
+        public Hooman()
+        {
 
         }
 
+        // Constructor with one argument
+        public Hooman(string name)
+        {
+            this.name = name;
+        }
 
+        // Constructor with 2 arguments
+        public Hooman(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
 
-        internal class Program
-        { 
-    
-            static void Main(string[] args)
-            {
-               // Create (instantiate) a hooman object from Hooman class
-                Hooman owner = new Hooman("Ossi Omistaja", 35, "isäntä");
+        // Constructor with 3 arguments
+        public Hooman(string name, int age, string gender)
+        {
+            this.name = name;
+            this.age = age;
+            this.gender = gender;
 
-                // Call the SayOpinion method
-                owner.SayOpinion();
+        }
 
-                // Save owners name property to a variable
-                string who = owner.name;
+        // A method to say something
+        public void SayOpinion()
+        {
+            Console.WriteLine("Voi, lemmikit ne on elämän suola");
+        }
+    }
 
-                // Test it by echoing it to a console
-                Console.WriteLine("totesi " + who);
+    class CatOwner : Hooman
+    {
+        public new void SayOpinion()
+        {
+            Console.WriteLine("Kissat ovat itsenäisiä ja pitävät hiiret loitolla");
+        }
+    }
 
-                // Create a new catowner object
-                CatOwner catOwner = new CatOwner();
+    class DogOwner : Hooman
 
-                // Use catowners SayOpinion method
-                catOwner.SayOpinion();
+    {
+        public new void SayOpinion()
+        {
+            Console.WriteLine("Koira on uskollinen ja luotettava kumppani, ihmisen paras ystävä");
+        }
+    }
 
-                // Keep the window open until enter pressed
-                Console.ReadLine(); 
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create (instantiate) a hooman object from Hooman class
+            Hooman owner = new Hooman("Ossi Omistaja", 35, "isäntä");
 
-                
+            // Call the SayOpinion method
+            owner.SayOpinion();
 
-            
-            }
-        } 
+            // Save owners name property to a variable
+            string who = owner.name;
+
+            // Test it by echoing it to a console
+            Console.WriteLine("totesi " + who);
+
+            // Create a new catowner object
+            CatOwner catOwner = new CatOwner();
+
+            // Use catowners SayOpinion method
+            catOwner.SayOpinion();
+
+            // Create a new dog owner and call SayOpinion method
+            DogOwner dogOwner = new DogOwner();
+            dogOwner.SayOpinion();
+
+            // Keep the window opent until enter pressed
+            Console.ReadLine();
+        }
+    }
 }
